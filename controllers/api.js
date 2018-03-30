47f6d0d0854e5bca9182174730bc8fe97a6960f5
YooHannah <1942063705@qq.com>
2018-03-30 19:31:51 +0800

    just test

--- controllers/api.js
+++ controllers/api.js
@@ -3,12 +3,12 @@
 const APIError = require('../rest').APIError;
 
 module.exports = {
-     'GET /': async (ctx,next) =>{
+     'GET /api': async (ctx,next) =>{
         console.log(ctx.request.body);
         let p = {data:'ok',color:'red'};
         ctx.rest(p)
     },
-    'POST /api/userInfo': async (ctx,next) =>{
+    'GET /api/userInfo': async (ctx,next) =>{
         console.log(ctx.request.body);
         let p = {data:'ok',color:'red'};
         ctx.rest(p)
