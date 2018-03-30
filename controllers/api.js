6692cd91c2e366d4561fbc9b9c109d2e29cf61ed
YooHannah <1942063705@qq.com>
2018-03-30 19:00:14 +0800

    just test

--- controllers/api.js
+++ controllers/api.js
@@ -7,7 +7,7 @@ module.exports = {
         console.log(ctx.request.body);
         let p = {data:'ok',color:'red'};
         ctx.rest(p)
-    }
+    },
     'POST /api/userInfo': async (ctx,next) =>{
         console.log(ctx.request.body);
         let p = {data:'ok',color:'red'};
